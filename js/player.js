163fcb93d79b9083507a95e7a58b57af43573c9b
dreamer <valsa_de_euridice@naver.com>
2020-06-24 13:13:23 +0900

    차트추가

--- js/player.js
+++ js/player.js
@@ -1,4 +1,43 @@
 var player;
+var currentPlayingIdx;
+const API_Server = new URL('http://localhost:8080/');
+
+// TYPE별 음악 차트를 ajax로 가져오고, 화면에 태그를 뿌려주는 function 
+function getMusicChartList(type, page) {
+    console.log("type체크")
+    console.log(type)
+    $('html').scrollTop(0);
+
+    function success(result) {
+        let $musicChartDiv = $('.bottom');
+        $musicChartDiv.html(makeMusicChartListTag(result.content));
+
+        $('.chartType').each(function (index, item) {
+            if (item.dataset.id != type) {
+                $('[data-id="' + item.dataset.id + '"]').removeClass('clicked');
+            } else {
+                $('[data-id="' + item.dataset.id + '"]').addClass('clicked');
+            }
+        })
+    }
+
+    ajaxService.getAjax(API_Server.commonURL + 'musicChartList/' + type + '/' + page, success);
+
+}
+
+
+// 음악 차트 리스트 태그를 만들어서 문자열 형태로 반환하는 function
+function makeMusicChartListTag(song) {
+    let listStr = '';
+    song.forEach((song, index) => {
+        listStr += '<ul><li class="w20">' + song.ranking + '</li>'
+            + '<li class="w50 rd"><img src="' + song.thumbnail + '" width="40px" alt="album art"></li>'
+            + '<li class="w140 lf">' + song.title + '<div class="artist">' + song.singer + '</div></li>'
+            + '<li class="w28 click" data-sid="' + song.sid + '"><img src="play_list.png" alt="재생" width="12px"></li>'
+            + '<li class="w28 click" data-sid="' + song.sid + '"><img src="plus_list.png" alt="추가" width="12px"></li></ul>'
+    })
+    return listStr;
+}
 
 $(document).ready(() => {
     // 2. This code loads the IFrame Player API code asynchronously.
@@ -6,8 +45,32 @@ $(document).ready(() => {
     tag.src = "https://www.youtube.com/iframe_api";
     var firstScriptTag = document.getElementsByTagName('script')[0];
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    getMusicChartList('realtime',1);
     onPlay();
-})
+
+    $('#chartTypes').on('click', 'li', function (e) {
+        // 로고 and 로그인 버튼 클릭 이벤트 설정하기
+        e.preventDefault;
+        let type = this.dataset.id;
+        let $chartText = $("#chartText");
+        if (type != null) {
+            console.log('null이 아님')
+            if (type == 'realtime') {
+                $chartText.text('실시간차트')
+            } else if (type == 'daily') {
+                $chartText.text('일간차트')
+            } else if (type == 'weekly') {
+                $chartText.text('주간차트')
+            } else {
+                $chartText.text('월간차트')
+            }
+            getMusicChartList(type, 1);
+        }
+    })
+
+
+}) // end of onready 
+
 
 
 function onPlay(targetIdx) {
